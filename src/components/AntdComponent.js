fd158c44325bea36cd1dc7ca2e06d5150ed18d7e
Chinseok Lee <me@askcompany.kr>
2019-08-09 00:22:40 +0900

    antd Table 예시 추가

--- src/components/AntdComponent.js
+++ src/components/AntdComponent.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import Button from 'antd/es/button';
+import Table from 'antd/es/table';
+
+
+const TableContainer = () => {
+    const dataSource = [];
+    for(let i=0; i<100; i++) {
+        dataSource.push({
+            key: i,
+            name: `Mike #${i}`,
+            age: 32,
+            address: '10 Downing Street'
+        });
+    }
+
+    const columns = [
+        { title: 'Name', dataIndex: 'name', key: 'name' },
+        { title: 'Age', dataIndex: 'age', key: 'age' },
+        { title: 'Address', dataIndex: 'address', key: 'address' },
+    ];
+    return <Table dataSource={dataSource} columns={columns} />;
+};
 
 
 const AntdComponent = () => {
@@ -8,6 +29,8 @@ const AntdComponent = () => {
             <h2>antd</h2>
             <Button>Button</Button>
             <Button type="primary">Primary Button</Button>
+            <hr/>
+            <TableContainer />
         </div>
     );
 };
