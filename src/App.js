8cc00391ccb1ad4450228bfab03fa2d3d6aafd95
Chinseok Lee <me@askcompany.kr>
2019-08-09 00:01:18 +0900

    antd button 예시 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './App.css';
 import UseInputContainer from "./components/UseInputContainer";
 import UsePromiseContainer from "./components/UsePromiseContainer";
 import RootComponent from './react-redux-hooks/components';
+import AntdComponent from "./components/AntdComponent";
 
 function App() {
   return (
       <>
           <RootComponent />
+          <hr/>
+          <AntdComponent />
           <hr/>
           <UseInputContainer />
           <hr/>
---
+++ src/components/AntdComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'antd/es/button';
+
+
+const AntdComponent = () => {
+    return (
+        <div>
+            <h2>antd</h2>
+            <Button>Button</Button>
+            <Button type="primary">Primary Button</Button>
+        </div>
+    );
+};
+
+
+export default AntdComponent;
